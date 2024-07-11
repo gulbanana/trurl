520d90e67fd9f3a6738becb39b11cd7b76ba4236
Thomas Castiglione <castiglione@mac.com>
2024-07-11 19:50:55 +0800

    Program.cs

--- Program.cs
+++ Program.cs
@@ -1,36 +1,29 @@
 ﻿using IrcDotNet;
 using Microsoft.Extensions.Configuration;
 using System.Linq;
+using trurl;
 
-namespace trurl;
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        var config = new ConfigurationBuilder()
-            .AddJsonFile("config.json")
+var config = new ConfigurationBuilder()
+    .AddJsonFile("config.json")
 #if DEBUG
-            .AddJsonFile("config.Development.json")
+    .AddJsonFile("config.Development.json")
 #endif
-            .Build();                         
+    .Build();                         
 
-        var server = config["server"];
-        var nick = config["nick"];
-        var channels = config["channels"];
-        var owner = config["owner"];
-        var admins = config["admins"];
+var server = config["server"];
+var nick = config["nick"];
+var channels = config["channels"];
+var owner = config["owner"];
+var admins = config["admins"];
 
-        var bot = new DiceBot(args.Contains("--daemon"), owner, admins.Split(','));
+var bot = new DiceBot(args.Contains("--daemon"), owner, admins.Split(','));
         
-        var userInfo = new IrcUserRegistrationInfo()
-        {
-            NickName = nick,
-            UserName = nick,
-            RealName = "Trurl Klapaucius, Cyberiad Dicebot"
-        };
+var userInfo = new IrcUserRegistrationInfo()
+{
+    NickName = nick,
+    UserName = nick,
+    RealName = "Trurl Klapaucius, Cyberiad Dicebot"
+};
 
-        //blocks until bot.Stop() is called or 'exit' is entered on the commandline
-        bot.Run(server, userInfo, channels.Split(','));
-    }
-}
+//blocks until bot.Stop() is called or 'exit' is entered on the commandline
+bot.Run(server, userInfo, channels.Split(','));
